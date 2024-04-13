8ca6d27054e0f37290732c11972805abc6632b21
Sobhan Ranjbar <sobhanranjbar2@gmail.com>
2024-04-13 11:58:19 -0600

    bug fix from chisel part

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import subprocess
-while True :
-    print("1. Linux Hawshemi optimizer ")
-    print("2. Linux OPIRAN optimizer ")
-    print("3. chisel tunnel ")
-    print("4. rathole compile ")
-    print("5. rathole install ")
-    print("0. exit")
-    print("\n enter your number :",end="")
-    num = int(input())
-    if num == 1:
-        subprocess.run(["chmod", '+x', '/Linux-Optimizer/linux-optimizer.sh'])
-        subprocess.run(['bash',"./Linux-Optimizer/linux-optimizer.sh"])
-    if num == 2:
-        subprocess.run(["chmod", '+x', '/VPS-Optimizer/optimizer.sh'])
-        subprocess.run(['bash', "./VPS-Optimizer/optimizer.sh"])
-    if num == 3:
-        subprocess.run(["chmod", '+x', '/Chisel_multipleServers/chisel.py'])
-        subprocess.run(['python3', "/Chisel_multipleServers/chisel.py"])
-    if num == 0:
-        break
+import sys
+def chisel():
+    from Chisel_multipleServers import chisel
+if __name__ == '__main__':
+    while True :
+        print("1. Linux Hawshemi optimizer ")
+        print("2. Linux OPIRAN optimizer ")
+        print("3. chisel tunnel ")
+        print("4. rathole compile ")
+        print("5. rathole install ")
+        print("0. exit")
+        print("\n enter your number :",end="")
+        num = int(input())
+        if num == 1:
+            subprocess.run(["chmod", '+x', '/Linux-Optimizer/linux-optimizer.sh'])
+            subprocess.run(['bash',"./Linux-Optimizer/linux-optimizer.sh"])
+        if num == 2:
+            subprocess.run(["chmod", '+x', '/VPS-Optimizer/optimizer.sh'])
+            subprocess.run(['bash', "./VPS-Optimizer/optimizer.sh"])
+        if num == 3:
+            chisel()
+        if num == 0:
+            break
