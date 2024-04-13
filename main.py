ae230441e54bd78332fa04d7b27658116d82be26
Sobhan Ranjbar <sobhanranjbar2@gmail.com>
2024-04-13 01:08:29 -0600

    Add OPIRAN optimizer

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ while True :
     print("\n enter your number :",end="")
     num = int(input())
     if num == 1:
-        subprocess.run(["chmod",'+x','main.py'])
         subprocess.run(["chmod", '+x', '/Linux-Optimizer/linux-optimizer.sh'])
         subprocess.run(['bash',"./Linux-Optimizer/linux-optimizer.sh"])
+    if num == 2:
+        subprocess.run(["chmod", '+x', '/VPS-Optimizer/optimizer.sh'])
+        subprocess.run(['bash', "./VPS-Optimizer/optimizer.sh"])
     if num == 0:
         break
