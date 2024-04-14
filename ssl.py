5e8aaf04bb38d146e6b7efd07c852bb08621ddc1
Sobhan Ranjbar <sobhanranjbar2@gmail.com>
2024-04-14 14:39:41 -0600

    fix bugs in ssl part

--- ssl.py
+++ ssl.py
@@ -15,29 +15,30 @@ def takessl():
         print("\ncurl installed successful\n")
     else:
         print("\nunable to install curl !\n")
-    command1_process = subprocess.run(['sudo', "curl", 'https://get.acme.sh','|','sh'])
+    command1_process = subprocess.run(['sudo', "wget", 'https://get.acme.sh'])
     if command1_process.returncode == 0 :
         print("\ncommand1 was successful\n")
     else:
         print("\nunable to do command1\n")
-    command2_process = subprocess.run(['sudo', "~/.acme.sh/acme.sh", '--set-default-ca','--server','letsencrypt'])
+    runsh_process = subprocess.run(['sudo', 'sh','index.html'])
+    command2_process = subprocess.run(['sudo', "/root/.acme.sh/acme.sh", '--set-default-ca','--server','letsencrypt'])
     if command2_process.returncode == 0 :
         print("\ncommand2 was successful\n")
     else:
         print("\nunable to do command2\n")
-    command3_process = subprocess.run(['sudo', "~/.acme.sh/acme.sh", '--register-account', '-m', 'mannemikhambasham@gmail.com'])
+    command3_process = subprocess.run(['sudo', "/root/.acme.sh/acme.sh", '--register-account', '-m', 'mannemikhambasham@gmail.com'])
     if command3_process.returncode == 0 :
         print("\ncommand3 was successful\n")
     else:
         print("\nunable to do command3\n")
     print("enter your sub-domain :",end="")
     domain=input()
-    command4_process = subprocess.run(['sudo', "~/.acme.sh/acme.sh", '--issue', '-d', domain , "--standalone"])
+    command4_process = subprocess.run(['sudo', "/root/.acme.sh/acme.sh", '--issue', '-d', domain , "--standalone"])
     if command4_process.returncode == 0 :
         print("\ncommand4 was successful\n")
     else:
         print("\nunable to do command4\n")
-    command5_process = subprocess.run(['sudo', "~/.acme.sh/acme.sh", '--installcert', '-d', domain, "--key-file","/root/private.key","--fullchain-file","/root/cert.crt"])
+    command5_process = subprocess.run(['sudo', "/root/.acme.sh/acme.sh", '--installcert', '-d', domain, "--key-file","/root/private.key","--fullchain-file","/root/cer>
     if command5_process.returncode == 0 :
         print("\ncommand5 was successful\n")
     else:
