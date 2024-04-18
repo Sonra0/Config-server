1c0a989d675ab7bc18111f9284215715430985fa
SONRA <166879455+Sonra0@users.noreply.github.com>
2024-04-18 00:15:52 -0600

    Update backup-xui.py

--- backup-xui.py
+++ backup-xui.py
@@ -1,3 +1,27 @@
 import subprocess
+import os
+import shutil
 def backup():
-    subprocess.run(['sudo', "apt", "-y","update"])
+    os.chdir('/root/')
+    try :
+        shutil.rmtree('/root/backup-xui/', ignore_errors=True)
+    finally:
+        os.mkdir("/root/backup-xui/")
+        shutil.copyfile("/root/Config-server/backup.py", "/root/backup-xui/backup.py")
+        subprocess.run(["apt", '-y', 'update'])
+        subprocess.run(["apt", 'install', 'python3'])
+        subprocess.run(["apt", 'install', 'cron'])
+        subprocess.run(["apt", 'install', 'python3-pip'])
+        subprocess.run(["apt", 'install', 'pyTelegramBotAPI'])
+        print("\nEnter your telegram bot token :",end="")
+        token = input()
+        print("\nEnter your telegram account chat id :", end="")
+        chat_id = input()
+        script_path = '/root/backup-xui/backup.py'
+        cron_job = f'\n0 * * * * /usr/bin/python3 {script_path}\n'
+        try :
+            with open('/etc/cron.d/mycron', 'w') as file:
+                file.write(cron_job)
+        except :
+            print ("cronjob failed")
+        subprocess.run(["python3", "backup.py"])
