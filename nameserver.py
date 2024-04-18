42caf974aab1419528b68a190c2fb84bb3138dac
Sobhan Ranjbar <sobhanranjbar2@gmail.com>
2024-04-18 17:52:28 -0600

    add dns google for test

--- nameserver.py
+++ nameserver.py
@@ -10,3 +10,21 @@ def dns():
         file = open('/etc/resolv.conf', 'w')
         file.write("nameserver 8.8.8.8\nnameserver 8.8.4.4")
         file.close()
+        print("DNS sets successfully")
+    if num2 == 2 :
+        file = open('/etc/resolv.conf', 'w')
+        file.write("nameserver 1.1.1.2\nnameserver 1.0.0.2")
+        file.close()
+        print("DNS sets successfully")
+    if num2 == 3 :
+        file = open('/etc/resolv.conf', 'w')
+        file.write("nameserver 178.22.122.100\nnameserver 185.51.200.2")
+        file.close()
+        print("DNS sets successfully")
+    if num2 == 4 :
+        file = open('/etc/resolv.conf', 'w')
+        file.write("nameserver 78.157.42.100\nnameserver 78.157.42.101")
+        file.close()
+        print("DNS sets successfully")
+    else :
+        continue
