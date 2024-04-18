9974fdd43af71ab484ffb0deb59a8447f61dc375
SONRA <166879455+Sonra0@users.noreply.github.com>
2024-04-18 00:50:39 -0600

    Update backup_xui.py

--- backup_xui.py
+++ backup_xui.py
@@ -8,15 +8,12 @@ def backup_do():
     finally:
         os.mkdir("/root/backup-xui/")
         shutil.copyfile("/root/Config-server/backup.py", "/root/backup-xui/backup.py")
+        shutil.copyfile("/root/Config-server/data.py", "/root/backup-xui/data.py")
         subprocess.run(["apt", '-y', 'update'])
         subprocess.run(["apt", 'install', 'python3'])
         subprocess.run(["apt", 'install', 'cron'])
         subprocess.run(["apt", 'install', 'python3-pip'])
         subprocess.run(["pip", 'install', 'pyTelegramBotAPI'])
-        print("\nEnter your telegram bot token :",end="")
-        token = input()
-        print("\nEnter your telegram account chat id :", end="")
-        chat_id = input()
         script_path = '/root/backup-xui/backup.py'
         cron_job = f'\n0 * * * * /usr/bin/python3 {script_path}\n'
         try :
