51bcb74a5ced5ba8e0ced3e20268d06c0d3b252d
Sobhan Ranjbar <sobhanranjbar2@gmail.com>
2024-04-18 22:14:56 -0600

    fix code

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import subprocess
-import sys
+
 from ufwset import ufwrun
 from ssl import takessl
 from backup_xui import backup_do
 from firewall_iran import firewall
 from nameserver import dns
+
+
 def chisel():
-    from Chisel_multipleServers import chisel
+    from Chisel_multipleServers import chisel  # noqa
+
+
 def hawshemi():
     chmod_process = subprocess.run(["chmod", '+x', '/Linux-Optimizer/linux-optimizer.sh', ], capture_output=True,
                                    text=True)
@@ -15,6 +19,8 @@ def hawshemi():
     else:
         print("\nChmod failed\n")
     run_process = subprocess.run(['bash', "./Linux-Optimizer/linux-optimizer.sh"])
+
+
 def op_iran():
     chmod_process = subprocess.run(["chmod", '+x', '/VPS-Optimizer/optimizer.sh'], capture_output=True, text=True)
     if chmod_process.returncode == 0:
@@ -22,6 +28,8 @@ def op_iran():
     else:
         print("\nChmod failed\n")
     run_process = subprocess.run(['bash', "./VPS-Optimizer/optimizer.sh"])
+
+
 def rathole_compile():
     chmod_process = subprocess.run(["chmod", '+x', '/Rathole_reverseTunnel/install.sh'], capture_output=True, text=True)
     if chmod_process.returncode == 0:
@@ -29,6 +37,8 @@ def rathole_compile():
     else:
         print("\nchmod failed\n")
     run_process = subprocess.run(['bash', "./Rathole_reverseTunnel/install.sh"])
+
+
 def rathole_install():
     chmod_process = subprocess.run(["chmod", '+x', '/Rathole_reverseTunnel/go.sh'], capture_output=True, text=True)
     if chmod_process.returncode == 0:
@@ -36,20 +46,28 @@ def rathole_install():
     else:
         print("\nchmod failed\n")
     run_process = subprocess.run(['bash', "./Rathole_reverseTunnel/go.sh"])
+
+
 def run_private():
-    chmod_process = subprocess.run(["chmod", '+x', '/root/Config-server/PrivateIP_TCP-UDP_Tunnel/Private.sh'],capture_output=True, text=True)
+    chmod_process = subprocess.run(["chmod", '+x', '/root/Config-server/PrivateIP_TCP-UDP_Tunnel/Private.sh'],
+                                   capture_output=True, text=True)
     if chmod_process.returncode == 0:
         print("\nchmod successful.\n")
     else:
         print("\nchmod failed\n")
     run_process = subprocess.run(['bash', "/root/Config-server/PrivateIP_TCP-UDP_Tunnel/Private.sh"])
+
+
 def run_gost():
-    chmod_process = subprocess.run(["chmod", '+x', '/root/Config-server/Gost-ip6/Gost.sh'], capture_output=True,text=True)
+    chmod_process = subprocess.run(["chmod", '+x', '/root/Config-server/Gost-ip6/Gost.sh'], capture_output=True,
+                                   text=True)
     if chmod_process.returncode == 0:
         print("\nchmod successful.\n")
     else:
         print("\nchmod failed\n")
     run_process = subprocess.run(['bash', "/root/Config-server/Gost-ip6/Gost.sh"])
+
+
 def install_mh():
     print("enter your version:( example: 2.2.1 ):", end=' ')
     version = 'v' + input()
@@ -59,10 +77,14 @@ def install_mh():
     else:
         print("\nchmod failed\n")
     run_process = subprocess.run(['bash', "./3x-ui/install.sh", version])
+
+
 def install_alireza():
     print("enter your version:( example: 2.2.1 ):", end=' ')
     version = input()
     run_process = subprocess.run(['bash', "./x-ui/install.sh", version])
+
+
 def change_ssh():
     chmod_process = subprocess.run(["chmod", '+x', 'change-ssh-port.sh'], capture_output=True, text=True)
     if chmod_process.returncode == 0:
@@ -70,9 +92,12 @@ def change_ssh():
     else:
         print("\nchmod failed\n")
     run_process = subprocess.run(['bash', "change-ssh-port.sh"])
+
+
 if __name__ == '__main__':
-    while True :
-        subprocess.run(['curl', "-s","https://raw.githubusercontent.com/shinya/pokemon-terminal-art/main/256color/silver/001.txt"])
+    while True:
+        subprocess.run(['curl', "-s",
+                        "https://raw.githubusercontent.com/shinya/pokemon-terminal-art/main/256color/silver/001.txt"])
         print("\n1. Linux optimizer ")
         print("2. Tunnel server ")
         print("3. Install x-ui panel")
@@ -83,13 +108,13 @@ if __name__ == '__main__':
         print("8. X-UI Backup")
         print("9. Change NameServer")
         print("0. Exit")
-        print("\nEnter your number:",end=" ")
+        print("\nEnter your number:", end=" ")
         num = int(input())
         if num == 1:
             print("\n1. Hawshemi optimizer")
             print("2. OPIRAN optimizer")
             print("0. Exit")
-            print("\nEnter your number:",end=" ")
+            print("\nEnter your number:", end=" ")
             num2 = int(input())
             if num2 == 1:
                 hawshemi()
@@ -97,64 +122,64 @@ if __name__ == '__main__':
                 op_iran()
             if num2 == 0:
                 continue
-        if num == 2 :
+        if num == 2:
             print("\n1. Rathole")
             print("2. Chisel")
             print("3. Gost + PrivateIP")
             print("0. Exit")
             print("\nEnter your number:", end=" ")
             num2 = int(input())
-            if num2 == 1 :
+            if num2 == 1:
                 print("\n1. Compile Rathole")
                 print("2. Setup Rathole")
                 print("0. Exit")
-                print("\nEnter your number:",end=" ")
-                num3=int(input())
-                if num3 == 1 :
+                print("\nEnter your number:", end=" ")
+                num3 = int(input())
+                if num3 == 1:
                     rathole_compile()
-                if num3 == 2 :
+                if num3 == 2:
                     rathole_install()
-                if num3 == 0 :
+                if num3 == 0:
                     continue
-            if num2 == 2 :
+            if num2 == 2:
                 chisel()
             if num2 == 3:
                 print("\n1. PrivateIP")
                 print("2. Gost")
                 print("0. Exit")
-                print("\nEnter your number: ",end='')
+                print("\nEnter your number: ", end='')
                 num3 = int(input())
-                if num3 == 1 :
+                if num3 == 1:
                     run_private()
-                if num3 == 2 :
+                if num3 == 2:
                     run_gost()
-                if num3 == 0 :
+                if num3 == 0:
                     continue
-            if num2 == 0 :
+            if num2 == 0:
                 continue
-        if num == 3 :
+        if num == 3:
             print("\n1. Mhsanaei")
             print("2. Alireza")
             print("0. Exit")
-            print("\nEnter your number:",end=" ")
+            print("\nEnter your number:", end=" ")
             num2 = int(input())
-            if num2 == 1 :
+            if num2 == 1:
                 install_mh()
-            if num2 == 2 :
+            if num2 == 2:
                 install_alireza()
-            if num2 == 0 :
+            if num2 == 0:
                 continue
-        if num == 4 :
+        if num == 4:
             ufwrun()
-        if num == 5 :
+        if num == 5:
             change_ssh()
-        if num == 6 :
+        if num == 6:
             takessl()
-        if num == 7 :
+        if num == 7:
             firewall()
-        if num == 8 :
+        if num == 8:
             backup_do()
-        if num == 9 :
+        if num == 9:
             dns()
         if num == 0:
             break
